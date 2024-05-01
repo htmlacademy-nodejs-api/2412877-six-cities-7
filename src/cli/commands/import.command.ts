a61d42d1186b0afcf01f74c1cd4582dff25c5ff9
Art <102982@inbox.ru>
2024-05-01 15:33:40 +0500

    Ref import

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,20 +1,27 @@
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { OfferRent } from '../../shared/types/index.js';
 import { Command } from './command.interface.js';
 
 export class ImportCommand implements Command {
+  private onReadOffer(offer: OfferRent): void {
+    console.info(offer);
+  }
+
   public getName(): string {
     return '--import';
   }
 
   public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
-    const fileReader = new TSVFileReader(filename.trim());
-
     try {
+      const fileReader = new TSVFileReader(filename.trim());
+      fileReader.on('read', this.onReadOffer);
       fileReader.read();
-      console.log(fileReader.toArray());
     } catch (error) {
       console.error(`Не удалось прочитать данные из файла ${filename}`);
+      if (error instanceof Error) {
+        console.error(`Детали: ${error.message}`);
+      }
     }
   }
 }
